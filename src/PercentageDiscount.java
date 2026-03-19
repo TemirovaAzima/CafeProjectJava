e3827f15cba85cb19f78963e35dc4203b7441dec
TemirovaAzima <temirovaazima9@gmail.com>
2026-03-19 18:53:08 +0100

    PercentageDiscount inherited discount class and implemented abstract (apply) method

--- src/PercentageDiscount.java
+++ src/PercentageDiscount.java
@@ -1,1 +1,21 @@
+public class PercentageDiscount extends Discount {
+
+    public final double percentage;
+
+    public PercentageDiscount(double percentage){
+        super(String.format("%.1f%% off",percentage));
+        if(percentage<=0 || percentage>100)
+            throw new IllegalArgumentException("Discount percentage must be in the range [0,100]. Received" + percentage);
+        this.percentage = percentage;
+    }
+    public double getPercentage() {
+        return percentage;
+    }
+
+    @Override
+    double apply(double originalPrice) {
+        double discounted = originalPrice * (1 - percentage/100.0);
+        return Math.max(discounted,0);
+    }
+}
 
